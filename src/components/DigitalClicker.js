a2bcdbdb0a7a62c883b94c1775310c483e025afe
Doug Kintop <dkintop@hotmail.com>
2020-01-26 14:03:29 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,4 +1,15 @@
 // Code DigitalClicker Component Here
 import { React } from "react";
 
-export default class DigitalClicker extends React.Component
+export default class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  render() {
+    <button></button>;
+  }
+}
